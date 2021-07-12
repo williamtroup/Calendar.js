23a853351cc418d57981cd313911daa2e611f2b5
William Troup <williamtroup@gmail.com>
2021-07-12 09:04:18 +0100

    Reverted previous date changes.

--- src/calendarjs.js
+++ src/calendarjs.js
@@ -1006,12 +1006,12 @@ function calendarJs( id, options, startDateTime ) {
 
     function setSelectedDate( date, input ) {
         if ( isDefined( date ) ) {
-            if ( input.type === "date" ) {
-                input.valueAsDate = new Date( Date.UTC( date.getFullYear(), date.getMonth(), date.getDate() ) );
-            } else {
-                var day = ( "0" + date.getDate() ).slice( -2 ),
-                    month = ( "0" + ( date.getMonth() + 1 ) ).slice( -2 );
+            var day = ( "0" + date.getDate() ).slice( -2 ),
+                month = ( "0" + ( date.getMonth() + 1 ) ).slice( -2 );
 
+            if ( input.type === "date" ) {
+                input.value = date.getFullYear() + "-" + month + "-" + day;
+            } else {
                 input.value = day + "/" + month + "/" + date.getFullYear();
             }
         }
@@ -1022,7 +1022,7 @@ function calendarJs( id, options, startDateTime ) {
 
         if ( input.value !== "" ) {
             if ( input.type === "date" ) {
-                result = input.valueAsDate;
+                result = new Date( input.value + "T00:00:00Z" );
             } else {
     
                 var match = input.value.match( /^(\d{1,2})\/(\d{1,2})\/(\d{4})$/ );
