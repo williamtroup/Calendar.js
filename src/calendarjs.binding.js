f8f0ed60fa9053323bd848dc656d249d178cdfcc
William Troup <williamtroup@gmail.com>
2023-03-31 08:51:41 +0100

    Updated to v1.7.3.

--- src/calendarjs.binding.js
+++ src/calendarjs.binding.js
@@ -4,7 +4,7 @@
  * A drag & drop event calendar (for Javascript), that is fully responsive and compatible with all modern browsers.
  * 
  * @file        calendarjs.binding.js
- * @version     v1.7.2
+ * @version     v1.7.3
  * @author      Bunoon
  * @license     GNU AGPLv3
  * @copyright   Bunoon 2023
--- src/calendarjs.js
+++ src/calendarjs.js
@@ -4,7 +4,7 @@
  * A drag & drop event calendar (for Javascript), that is fully responsive and compatible with all modern browsers.
  * 
  * @file        calendarjs.js
- * @version     v1.7.2
+ * @version     v1.7.3
  * @author      Bunoon
  * @license     GNU AGPLv3
  * @copyright   Bunoon 2023
@@ -8064,7 +8064,7 @@ function calendarJs( elementOrId, options, searchOptions ) {
      * @returns     {string}                                                The version number.
      */
     this.getVersion = function() {
-        return "1.7.2";
+        return "1.7.3";
     };
 
     /**
