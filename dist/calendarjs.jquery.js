988ff78dc597843164e9843be66d57392cb5012f
William Troup <williamtroup@gmail.com>
2023-03-31 08:47:26 +0100

    "dist" now contains JS that contains no documentation version of the compiled files, "src" contains everything.

--- dist/calendarjs.jquery.js
+++ dist/calendarjs.jquery.js
@@ -1,34 +1,10 @@
-/**
- * Calendar.js
- * 
- * A drag & drop event calendar (for Javascript), that is fully responsive and compatible with all modern browsers.
- * 
- * @file        calendarjs.jquery.js
- * @version     v1.7.2
- * @author      Bunoon
- * @license     GNU AGPLv3
- * @copyright   Bunoon 2023
- */
-
-(function ( $ ) {
-    /**
-     * calendarJs().
-     * 
-     * Created new instances of the Calendar.js class for each element.
-     * 
-     * @public
-     * 
-     * @param       {Object}    options                                     All the configurable options that should be used (refer to "Options" documentation for properties).
-     * 
-     * @returns     {Object}                                                The Calendar.JS instances created.
-     */
-    $.fn.calendarJs = function( options ) {
-        var instances = [];
-        
-        this.each(function() {
-            instances.push( new calendarJs( this, options ) );
-        });
-        
-        return instances;
+/*! Calendar.js v1.7.2 | (c) Bunoon | GNU AGPLv3 License */
+(function($) {
+    $.fn.calendarJs = function(options) {
+      var instances = [];
+      this.each(function() {
+        instances.push(new calendarJs(this, options));
+      });
+      return instances;
     };
-} ( jQuery ) );
+  })(jQuery);
---
+++ src/calendarjs.jquery.js
@@ -0,0 +1,34 @@
+/**
+ * Calendar.js
+ * 
+ * A drag & drop event calendar (for Javascript), that is fully responsive and compatible with all modern browsers.
+ * 
+ * @file        calendarjs.jquery.js
+ * @version     v1.7.2
+ * @author      Bunoon
+ * @license     GNU AGPLv3
+ * @copyright   Bunoon 2023
+ */
+
+(function ( $ ) {
+    /**
+     * calendarJs().
+     * 
+     * Created new instances of the Calendar.js class for each element.
+     * 
+     * @public
+     * 
+     * @param       {Object}    options                                     All the configurable options that should be used (refer to "Options" documentation for properties).
+     * 
+     * @returns     {Object}                                                The Calendar.JS instances created.
+     */
+    $.fn.calendarJs = function( options ) {
+        var instances = [];
+        
+        this.each(function() {
+            instances.push( new calendarJs( this, options ) );
+        });
+        
+        return instances;
+    };
+} ( jQuery ) );
