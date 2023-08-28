a2455d1275d29a28c3134f937fb27610270e57e3
William Troup <williamtroup@gmail.com>
2023-08-28 15:37:11 +0100

    Updated to v2.2.1.

--- dist/calendar.js
+++ dist/calendar.js
@@ -1,4 +1,4 @@
-/*! Calendar.js v2.2.0 | (c) Bunoon | GNU AGPLv3 License */
+/*! Calendar.js v2.2.1 | (c) Bunoon | GNU AGPLv3 License */
 function calendarJs(elementOrId, options, searchOptions) {
   function build(newStartDateTime, fullRebuild, forceRefreshViews) {
     _currentDate = isDefinedDate(newStartDateTime) ? newStartDateTime : new Date();
@@ -7515,7 +7515,7 @@ function calendarJs(elementOrId, options, searchOptions) {
     }
   };
   this.getVersion = function() {
-    return "2.2.0";
+    return "2.2.1";
   };
   this.getId = function() {
     return _elementID;
