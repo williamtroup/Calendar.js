0847124b00473150ee077af80d4901df81886b52
William Troup <williamtroup@gmail.com>
2024-02-02 15:04:50 +0000

    Added tags for the languages.

--- dist/translations/calendar.translations.de.js
+++ dist/translations/calendar.translations.de.js
@@ -1,4 +1,4 @@
-/*! Calendar.js v2.10.7 | (c) Bunoon 2024 | MIT License */
+/*! Calendar.js v2.10.7 - German | (c) Bunoon 2024 | MIT License */
 var __TRANSLATION_OPTIONS = {
   "dayHeaderNames": [
     "Mo",
