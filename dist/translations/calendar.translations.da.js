8514c89124c9f107382bbcc01c145afde18a446a
William Troup <williamtroup@gmail.com>
2024-04-24 15:28:22 +0100

    Updated to v2.11.0.

--- dist/translations/calendar.translations.da.js
+++ dist/translations/calendar.translations.da.js
@@ -1,4 +1,4 @@
-/*! Calendar.js v2.10.15 - Danish | (c) Bunoon 2024 | MIT License */
+/*! Calendar.js v2.11.0 - Danish | (c) Bunoon 2024 | MIT License */
 var __TRANSLATION_OPTIONS = {
     "dayHeaderNames": [
         "man",
--- dist/translations/calendar.translations.et.js
+++ dist/translations/calendar.translations.et.js
@@ -1,4 +1,4 @@
-/*! Calendar.js v2.10.15 - Estonian | (c) Bunoon 2024 | MIT License */
+/*! Calendar.js v2.11.0 - Estonian | (c) Bunoon 2024 | MIT License */
 var __TRANSLATION_OPTIONS = {
     "dayHeaderNames": [
         "Esmasp",
