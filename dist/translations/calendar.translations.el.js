641110c9a2d20ea84ede787828a94f1ad95871e9
William Troup <williamtroup@gmail.com>
2024-05-31 14:54:00 +0100

    Updated to v2.11.0.

--- dist/translations/calendar.translations.el.js
+++ dist/translations/calendar.translations.el.js
@@ -1,4 +1,4 @@
-/*! Calendar.js v2.10.19 - Greek | (c) Bunoon 2024 | MIT License */
+/*! Calendar.js v2.11.0 - Greek | (c) Bunoon 2024 | MIT License */
 var __TRANSLATION_OPTIONS = {
     "dayHeaderNames": [
         "Δευτ",
--- src/calendar.js
+++ src/calendar.js
@@ -4,7 +4,7 @@
  * A javascript drag & drop event calendar, that is fully responsive and compatible with all modern browsers.
  * 
  * @file        calendar.js
- * @version     v2.10.19
+ * @version     v2.11.0
  * @author      Bunoon
  * @license     MIT License
  * @copyright   Bunoon 2024
@@ -13255,7 +13255,7 @@ function calendarJs( elementOrId, options, searchOptions ) {
      * @returns     {string}                                                The version number.
      */
     this.getVersion = function() {
-        return "2.10.19";
+        return "2.11.0";
     };
 
     /**
